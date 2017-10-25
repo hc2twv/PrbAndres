32fde6b2f86be106c1d66e493df73f67df671308
hc2twv <wavelasq@gmail.com>
2017-10-25 23:30:16 +0200

    Initial commit

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,8 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by hc2twv on 25/10/2017.
- * prueba github andres
  */
 public class Prueba {
+    public String id;
+    public String id1;
+    public String id2;
 }
